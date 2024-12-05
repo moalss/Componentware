0696de5188688768d32c43e9ffca644967fad3b9
Mohamed Issa <89844963+moalss@users.noreply.github.com>
2024-12-05 12:57:27 +0100

    Delete cw.firma directory

--- cw.firma/src/main/java/cw/cw/firma/repository/SensorDataRepository.java
+++
@@ -1,9 +0,0 @@
-package cw.cw.firma.repository;
-
-import cw.cw.firma.entity.SensorData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
-}
